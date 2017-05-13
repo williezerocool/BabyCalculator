19407b1f144a3a21a5ea1e4a5ac5e6e43696d221
Willie Suggs <klbooker09@gmail.com>
2017-05-12 23:09:47 -0500

    Add files via upload

---
+++ BabyCalculator.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package babycalculator;
+
+import java.util.Scanner;
+/**
+ *
+ * @author kendrabooker
+ */
+public class BabyCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner keyboard = new Scanner(System.in);
+
+                int d = 1;
+		double a, b, c;
+		String op;
+
+		do
+		{
+			System.out.print("> ");
+			a  = keyboard.nextDouble();
+                        if(a == 0){
+                        
+                            d = 0;
+                            break;
+                        }
+			op = keyboard.next();
+			b  = keyboard.nextDouble();
+
+			if ( op.equals("+") )
+				c = a + b;
+                        else if ( op.equals("-") )
+                                c = a - b;
+                        else if ( op.equals("/") )
+                                c = a / b;
+                        else if ( op.equals("*") )
+                                c = a * b;
+                        else{
+				System.out.println("Undefined operator: '" + op + "'.");
+				c = 0;
+			}
+
+			System.out.println(c);
+
+		} while ( d > 0 );
+	}
+}
